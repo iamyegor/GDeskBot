029efa805cbd9a2839cda000bf54352afae48441
yegor <astery227@gmail.com>
2024-07-30 11:54:49 +0300

    Remove Common from migrator

--- migrator/Common/AppEnvironment/EnvironmentResolver.cs
+++
@@ -1,8 +0,0 @@
-namespace Common.AppEnvironment;
-
-public class EnvironmentResolver
-{
-    public static bool IsDevelopment => !IsProduction;
-    public static bool IsProduction =>
-        Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") == "Production";
-}
